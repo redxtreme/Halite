77060291c4a1c18130ffb294c8bfcc2e48979700
redxtreme <zahopper@mtu.edu>
2018-01-10 22:14:54 -0600

    Added a flag so it doesn't produce replays

--- Halite2_ML_Advanced/data-creator.py
+++ Halite2_ML_Advanced/data-creator.py
@@ -25,7 +25,7 @@ for num in range(5000):
             p2_pct = round(player_2_wins/(player_1_wins+player_2_wins)*100.0, 2)
             print("Player 1 win: {}%; Player 2 win: {}%.".format(p1_pct, p2_pct))
 
-        os.system('./halite -d "360 240" "python MyBot-1.py" "python MyBot-2.py" >> data.gameout')
+        os.system('./halite -d "360 240" -r "python MyBot-1.py" "python MyBot-2.py" >> data.gameout')
 
         with open('data.gameout', 'r') as f:
             contents = f.readlines()
